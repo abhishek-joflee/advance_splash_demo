ca4a43891eb330a6ab1350318bc3284f2a3f4421
iAbhishekPatil <abhishek.patil@joflee.com>
2021-12-30 21:30:12 +0530

    ticker removed

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
@@ -8,51 +7,11 @@ class Splash extends StatefulWidget {
   State<Splash> createState() => _SplashState();
 }
 
-class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
-  late final Ticker _ticker;
-  Duration elapsed = Duration.zero;
-  @override
-  void initState() {
-    super.initState();
-    _ticker = createTicker((time) {
-      setState(() {
-        elapsed = time;
-      });
-    });
-    _ticker.start();
-  }
-
-  @override
-  void dispose() {
-    _ticker.dispose();
-    super.dispose();
-  }
-
+class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  const Text("mili: "),
-                  Text(elapsed.inMilliseconds.toString()),
-                ],
-              ),
-              Row(
-                children: [
-                  const Text("sec: "),
-                  Text(elapsed.inSeconds.toString()),
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
+    return const Scaffold(
+      body: Center(child: Text("HI")),
     );
   }
 }
