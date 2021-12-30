38b586d93e494d4cf58073d2f9e08b6a0153adac
iAbhishekPatil <abhishek.patil@joflee.com>
2021-12-30 21:07:19 +0530

    ticker setState timer added in splash

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,13 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
 
   @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  late final Ticker _ticker;
+  Duration elapsed = Duration.zero;
+  @override
+  void initState() {
+    super.initState();
+    _ticker = createTicker((time) {
+      setState(() {
+        elapsed = time;
+      });
+    });
+    _ticker.start();
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("This is Secondary splash screen"),
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  const Text("mili: "),
+                  Text(elapsed.inMilliseconds.toString()),
+                ],
+              ),
+              Row(
+                children: [
+                  const Text("sec: "),
+                  Text(elapsed.inSeconds.toString()),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
