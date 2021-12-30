ac434b90f0ce4d32c2315bedfdef07a9a74eb082
iAbhishekPatil <abhishek.patil@joflee.com>
2021-12-30 22:02:53 +0530

    image added in splash

--- lib/splash.dart
+++ lib/splash.dart
@@ -10,8 +10,15 @@ class Splash extends StatefulWidget {
 class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("HI")),
+    Size phoneSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Image(
+          height: phoneSize.height / 3,
+          width: phoneSize.height / 3,
+          image: const AssetImage('assets/images/flutter-icon.png'),
+        ),
+      ),
     );
   }
 }
